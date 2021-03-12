ee946a4fe30bc0005f639dede25f7e444157dc19
CristinaCasch <criscastro1926@hotmail.com>
2021-03-12 15:24:53 +0000

    Landing page

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = () => {
 	return (
-		<div class="card">
-			<img src="http://wikipedia.com" class="card-img-top" alt="..." />
+		<div class="card col">
+			<img src="..." class="card-img-top" alt="..." />
 			<div class="card-body">
 				<h5 class="card-title">Card title</h5>
 				<p class="card-text">
@@ -11,7 +11,7 @@ const Card = () => {
 					up the bulk of the card's content.
 				</p>
 				<a href="#" class="btn btn-primary">
-					Go somewhere
+					Find out move!
 				</a>
 			</div>
 		</div>
--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-	return <h1>Lo que quieras</h1>;
+	return (
+		<footer className="bg-dark text-center text-white">
+			<div className="text-center p-3">© 2020 Copyright:</div>
+		</footer>
+	);
 };
 
 export default Footer;
